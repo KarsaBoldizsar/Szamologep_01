5f4d1f259ca3b9e257809b30f8aa4260f6ca6c56
Diak <karsa.boldi@gmail.com>
2019-10-03 16:51:26 +0200

    android szamologep

---
+++ app/src/main/java/com/example/szamologep_01/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.szamologep_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+    /*
+    public TextView Felirat;
+    public TextView Eredmeny;
+    public
+*/
+}
